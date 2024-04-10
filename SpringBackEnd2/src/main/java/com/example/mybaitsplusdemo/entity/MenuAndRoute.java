aff5857d5bace7292e8f95553e50d60806ee6be3
ShirleyLiu2412 <yliu2412@uni.sydney.edu.au>
2024-04-11 01:51:28 +1000

    Fixed website code in w7
    Fixed front end, back end and database

--- SpringBackEnd2/src/main/java/com/example/mybaitsplusdemo/entity/MenuAndRoute.java
+++
@@ -1,6 +0,0 @@
-package com.example.mybaitsplusdemo.entity;
-
-import java.util.ArrayList;
-
-public record MenuAndRoute(ArrayList<Route1> routeList, ArrayList<Menu> menuTree) {
-}
--- SpringBackEnd2/src/main/java/com/example/mybaitsplusdemo/entity/Route1.java
+++
@@ -1,7 +0,0 @@
-package com.example.mybaitsplusdemo.entity;
-
-
-
-public record Route1(String path, String element) {
-
-}
